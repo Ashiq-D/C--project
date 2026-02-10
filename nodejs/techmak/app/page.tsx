96c259ffe80b85128abe0e593db3167cbe58a45c
Ashiq BIn Hamid <23-53342-3@student.aiub.edu>
2026-02-10 22:33:27 +0600

    folder created

---
+++ nodejs/techmak/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home =() =>{
+    return(
+        <div className="p-10 bg-shop-light-pink">
+          <h2 className="text-xl font-semibold">Home</h2>
+          <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos, error perferendis saepe ducimus voluptate necessitatibus consequuntur magnam architecto accusantium magni officiis, ea sapiente nostrum blanditiis, corrupti quo omnis repellat. Veniam aliquam ex voluptatem alias quidem ratione dolore est libero reprehenderit, non, totam asperiores, atque labore dignissimos quo eius excepturi similique.</h2>
+        
+        </div>
+    )
+};
+
+export default Home
